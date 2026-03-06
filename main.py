0460a708578759ef078bb413a9cdc72ad093b237
Bravnar <stan.muravyev@bluewin.ch>
2026-03-06 13:56:38 +0100

    finished chapter 1 LLMs

--- main.py
+++ main.py
@@ -2,28 +2,39 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
+MODEL = "gemini-2.5-flash"
 
 
 def get_args(desc="Default"):
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     return parser.parse_args()
 
 
-def generate_response(client):
-    prompt = get_args().user_prompt
-    model = "gemini-2.5-flash"
-    response = client.models.generate_content(model=model, contents=prompt)
+def return_verbose(response):
     metadata = response.usage_metadata
     if not metadata:
         raise RuntimeError("The model didn't wield a response")
-    answer_msg = [
-        f"User Prompt: {prompt}",
-        f"Prompt tokens: {metadata.prompt_token_count}",
-        f"Response tokens: {metadata.candidates_token_count}",
-        f"Response: {response.text}",
-    ]
-    return "\n".join(answer_msg)
+    return "\n".join(
+        [
+            "====================================================",
+            f"User prompt: {get_args().user_prompt}",
+            f"Prompt tokens: {metadata.prompt_token_count}",
+            f"Response tokens: {metadata.candidates_token_count}",
+            f"Response: {response.text}",
+            "====================================================",
+        ]
+    )
+
+
+def request_response(client):
+    prompt = get_args().user_prompt
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
+    response = client.models.generate_content(model=MODEL, contents=messages)
+    return return_verbose(response) if get_args().verbose else response.text
 
 
 def main():
@@ -34,7 +45,7 @@ def main():
             "The API Key was not found. Please ensure that you have a .env with your Google AI key"
         )
     client = genai.Client(api_key=api_key)
-    print(generate_response(client))
+    print(request_response(client))
 
 
 if __name__ == "__main__":
