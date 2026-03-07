66c21f6cbf36767f0475ca9a36aafe1554c00b88
Bravnar <stan.muravyev@bluewin.ch>
2026-03-07 10:37:02 +0100

    finished get_files_info.py and wrote tests for it

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,19 +2,27 @@ import os
 
 
 def get_files_info(working_directory, directory="."):
-    working_dir_abs = os.path.abspath(working_directory)
-    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
-    valid_target_dir = (
-        os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
-    )
-    print(target_dir)
-    if not valid_target_dir:
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    if os.path.isdir(directory):
-        return f'Error: "{directory}" is not a directory'
-    # for file in os.listdir(target_dir):
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+        valid_target_dir = (
+            os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+        )
+        if not valid_target_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+        ret = []
+        for file in os.listdir(target_dir):
+            file_path = os.path.join(target_dir, file)
+            ret.append(
+                f"- {file}: file_size={os.path.getsize(file_path)} bytes, is_dir={os.path.isdir(file_path)}"
+            )
+        return "\n".join(ret)
+    except Exception as e:
+        return f"Error: {e}"
 
 
 if __name__ == "__main__":
-    print("Running get_files_info tests:")
-    print(get_files_info("calculator", "pkg"))
+    # print("Running get_files_info tests:")
+    print(get_files_info("calculator"))
