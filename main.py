13cb1146f473416033b4966777cd7198396af21c
Bravnar <stan.muravyev@bluewin.ch>
2026-03-09 12:59:39 +0100

    functioning simplistic agent that runs 1 prompt at a time

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from google import genai
 from google.genai import types
 from prompts import system_prompt
 from call_function import available_functions
+from call_function import call_function
 
-MODEL = "gemini-2.5-flash"
+MODEL = "gemini-2.5-flash-lite"
 
 
 def get_args(desc="Default"):
@@ -16,34 +17,53 @@ def get_args(desc="Default"):
     return parser.parse_args()
 
 
-def return_verbose(response):
+def return_verbose(response, args):
     metadata = response.usage_metadata
     if not metadata:
         raise RuntimeError("The model didn't wield a response")
-    return "\n".join(
-        [
-            "====================================================",
-            f"User prompt: {get_args().user_prompt}",
-            f"Prompt tokens: {metadata.prompt_token_count}",
-            f"Response tokens: {metadata.candidates_token_count}",
-            f"{return_normal(response)}",
-            "====================================================",
-        ]
+    return (
+        "\n".join(
+            [
+                "====================================================",
+                f"User prompt: {args.user_prompt}",
+                f"Prompt tokens: {metadata.prompt_token_count}",
+                f"Response tokens: {metadata.candidates_token_count}",
+            ]
+        )
+        + f"\n{return_normal(response, True)}\n===================================================="
     )
 
 
-def return_normal(response):
+def return_normal(response, verbose_function=False):
     ret = []
     if response.function_calls:
         for function_call in response.function_calls:
-            ret.append(f"Calling function: {function_call.name}({function_call.args})")
+            function_call_result = call_function(
+                function_call, True if verbose_function else False
+            )
+            if not function_call_result.parts:
+                raise Exception(
+                    f"function call {function_call.name} result did not return parts"
+                )
+            function_response = function_call_result.parts[0].function_response
+            if not function_response:
+                raise Exception(
+                    f"function call {function_call.name} did not yield a response"
+                )
+            if not function_response.response:
+                raise Exception(
+                    f"The response field for {function_call.name} is missing!"
+                )
+            if verbose_function:
+                ret.append(f"-> {function_response.response}")
     else:
         ret.append(f"Response: {response.text}")
     return "\n".join(ret)
 
 
 def request_response(client):
-    prompt = get_args().user_prompt
+    args = get_args()
+    prompt = args.user_prompt
     messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
     response = client.models.generate_content(
         model=MODEL,
@@ -52,7 +72,7 @@ def request_response(client):
             tools=[available_functions], system_instruction=system_prompt
         ),
     )
-    return return_verbose(response) if get_args().verbose else return_normal(response)
+    return return_verbose(response, args) if args.verbose else return_normal(response)
 
 
 def main():
@@ -62,8 +82,11 @@ def main():
         raise RuntimeError(
             "The API Key was not found. Please ensure that you have a .env with your Google AI key"
         )
-    client = genai.Client(api_key=api_key)
-    print(request_response(client))
+    try:
+        client = genai.Client(api_key=api_key)
+        print(request_response(client))
+    except Exception as e:
+        print(f"Error: {e}")
 
 
 if __name__ == "__main__":
