f854abffbb948f3ec8dd9c799beb8cd58a0d4d5b
Bravnar <stan.muravyev@bluewin.ch>
2026-03-09 20:00:32 +0100

    finished base project - try to improve the verbose outputs and see how much better it can do

--- main.py
+++ main.py
@@ -17,50 +17,6 @@ def get_args(desc="Default"):
     return parser.parse_args()
 
 
-# def return_verbose(response, args):
-#     metadata = response.usage_metadata
-#     if not metadata:
-#         raise RuntimeError("The model didn't wield a response")
-#     return (
-#         "\n".join(
-#             [
-#                 "====================================================",
-#                 f"User prompt: {args.user_prompt}",
-#                 f"Prompt tokens: {metadata.prompt_token_count}",
-#                 f"Response tokens: {metadata.candidates_token_count}",
-#             ]
-#         )
-#         + f"\n{return_normal(response, True)}\n===================================================="
-#     )
-
-
-# def return_normal(response, verbose_function=False):
-#     ret = []
-#     if response.function_calls:
-#         for function_call in response.function_calls:
-#             function_call_result = call_function(
-#                 function_call, True if verbose_function else False
-#             )
-#             if not function_call_result.parts:
-#                 raise Exception(
-#                     f"function call {function_call.name} result did not return parts"
-#                 )
-#             function_response = function_call_result.parts[0].function_response
-#             if not function_response:
-#                 raise Exception(
-#                     f"function call {function_call.name} did not yield a response"
-#                 )
-#             if not function_response.response:
-#                 raise Exception(
-#                     f"The response field for {function_call.name} is missing!"
-#                 )
-#             if verbose_function:
-#                 ret.append(f"-> {function_response.response}")
-#     else:
-#         ret.append(f"Response: {response.text}")
-#     return "\n".join(ret)
-
-
 def process_output(response, verbose=False):
     function_responses = []
     for function_call in response.function_calls:
@@ -78,20 +34,6 @@ def process_output(response, verbose=False):
             raise Exception(f"The response field for {function_call.name} is missing!")
         function_responses.append(function_call_result.parts[0])
     return function_responses
-
-
-# def request_response(client):
-#     args = get_args()
-#     prompt = args.user_prompt
-#     messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
-#     response = client.models.generate_content(
-#         model=MODEL,
-#         contents=messages,
-#         config=types.GenerateContentConfig(
-#             tools=[available_functions], system_instruction=system_prompt
-#         ),
-#     )
-#     return return_verbose(response, args) if args.verbose else return_normal(response)
 
 
 def run_agent(client, messages):
@@ -138,7 +80,6 @@ def main():
         print(types._ListModelsParametersDict)
         client = genai.Client(api_key=api_key)
         print(loop_agent(client))
-        # print(request_response(client))
     except Exception as e:
         print(f"Error: {e}")
 
